2bc6ad6596f129134e3a21db821b2288188b366c
cutegirl520 <160213624+cutegirl520@users.noreply.github.com>
2021-08-17 20:01:02 +0000

    New transform repo

--- Assets/Robot/Scripts/UI/RobotUIController.cs
+++ Assets/Robot/Scripts/UI/RobotUIController.cs
@@ -45,4 +45,78 @@ public class RobotUIController : MonoBehaviour
 
     private void Awake()
     {
-    
+        upperLeg1Servo.servo = robot.legs[0].upperLeg;
+        lowerLeg1Servo.servo = robot.legs[0].lowerLeg;
+        upperLeg2Servo.servo = robot.legs[1].upperLeg;
+        lowerLeg2Servo.servo = robot.legs[1].lowerLeg;
+        upperLeg3Servo.servo = robot.legs[2].upperLeg;
+        lowerLeg3Servo.servo = robot.legs[2].lowerLeg;
+        upperLeg4Servo.servo = robot.legs[3].upperLeg;
+        lowerLeg4Servo.servo = robot.legs[3].lowerLeg;
+
+        toggleUpper.onValueChanged.AddListener(ToggleUpperLegs);
+        toggleLower.onValueChanged.AddListener(ToggleLowerLegs);
+
+        for (int i = 0; i < 4; i++)
+        {
+         //   robot.legs[i].upperLeg.SetAngle(Random.Range(-90.0f, 90.0f));
+        }
+        
+    }
+
+    private void Update()
+    {
+        if (!robot) return;
+        if (distanceText) distanceText.text = "Distance: " + robot.GetDistance().ToString("0.000") + "m";
+        if (headingText) headingText.text = "Heading: " + robot.GetHeading().ToString("00.0") + "°";
+
+        //StartCoroutine(SetAngleCoroutine());
+        desiredAngle = Random.Range(-30.0f, 30.0f); 
+        
+        if (angleReached == true)
+        {
+            StartCoroutine(moveRandomAngleCoroutine(desiredAngle));
+            angleReached = false;
+        }
+    }
+
+
+    //Function to move upperLeg0
+    public IEnumerator moveRandomAngleCoroutine(float desiredAngle)
+    {
+        robot.legs[0].upperLeg.SetAngle(desiredAngle);
+
+        print("Target: " + desiredAngle);
+        print("Current0: " + robot.legs[0].upperLeg.GetServoAngle());
+        while (true)
+        {
+            yield return null;
+            if (Mathf.Abs(robot.legs[0].upperLeg.GetServoAngle() - desiredAngle) <= 5)
+            {
+                angleReached = true;
+                print("Current: " + robot.legs[0].upperLeg.GetServoAngle());
+                break;
+            }
+        }
+    }
+
+    //private void trial_func()
+    //{
+    //    //Yield return suspends routine execution for given amount if seconds using scaled time
+    //    //If profile exisits use delay set there, else timescale set to 0
+
+    //    for (int i = 0; i < 4; i++)
+    //    {
+    //        float target = Random.Range(-90.0f, 90.0f);
+    //        robot.legs[i].upperLeg.SetAngle(target);
+
+    //      //  current_angle0 = robot.legs[0].upperLeg.GetServoAngle();
+    //      //  current_angle2 = robot.legs[2].upperLeg.GetServoAngle();
+
+    //        if (current_angle == target)
+    //        {
+
+    //        }
+
+    //        else
+ 
